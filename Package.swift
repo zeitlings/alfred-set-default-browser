e9fd4ca7b5db4e10c405b90e48a1c7ccf209b11e
zeitlings <patr.sy@gmail.com>
2023-01-19 18:01:16 +0100

    Remove ArgumentParser dependency
    swift-tools-version 5.7

--- Package.swift
+++ Package.swift
@@ -1,19 +1,18 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "set_default_browser",
-    dependencies: [
-		.package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
-    ],
+	platforms: [
+		.macOS(.v11)
+	],
+    dependencies: [],
     targets: [
         .executableTarget(
             name: "set_default_browser",
-            dependencies: [
-				.product(name: "ArgumentParser", package: "swift-argument-parser"),
-			]),
+            dependencies: []),
         .testTarget(
             name: "set_default_browserTests",
             dependencies: ["set_default_browser"]),
